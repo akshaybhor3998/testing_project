363639e481ccaf4dfa3e8770614dd388630c5fca
Akshay <Akshay@LAPTOP-TEE87BDK>
2024-06-12 00:09:01 +0530

    Add project files.

---
+++ testing_project/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Testing
+{
+    public static void Main(string[] args)
+    {
+        int[] data = { 1, 2, 3, 4, 5, 6,5 };
+        Array.Sort(data,(x,y)=>y.CompareTo(x));
+        foreach (var item in data)
+        {
+            Console.WriteLine(item + " ");
+        }
+    }
+}
