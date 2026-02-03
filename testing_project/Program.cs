26f6612cb123d58ca3d3d3b2a7e0fab3ca70f312
Akshay Bhor <akshaybhor3998@outlook.com>
2026-02-04 01:29:18 +0530

    Added Changes

--- testing_project/Program.cs
+++ testing_project/Program.cs
@@ -1,91 +1,16 @@
-﻿// Doubly Link list example
-
-using System;
-
-public class Node
+﻿public record Person
 {
-    public int data;
-    public Node prev;
-    public Node next;
-
-    public Node(int d)
-    {
-        data = d;
-        prev = null;
-        next = null;
-    }
-}
-
-public class DoublyLinkedList
-{
-    public Node head;
-
-    // Constructor
-    public DoublyLinkedList()
-    {
-        head = null;
-    }
-
-    // Add a node at the front of the list
-    public void InsertFront(int new_data)
-    {
-        Node new_node = new Node(new_data);
-        new_node.next = head;
-        if (head != null)
-            head.prev = new_node;
-        head = new_node;
-    }
-
-    // Add a node at the end of the list
-    public void InsertEnd(int new_data)
-    {
-        Node new_node = new Node(new_data);
-        Node last = head;
-        new_node.next = null;
-        if (head == null)
-        {
-            new_node.prev = null;
-            head = new_node;
-            return;
-        }
-        while (last.next != null)
-            last = last.next;
-        last.next = new_node;
-        new_node.prev = last;
-    }
-
-    // Print the doubly linked list
-    public void PrintList(Node node)
-    {
-        Node last = null;
-        Console.WriteLine("Traversal in forward direction: ");
-        while (node != null)
-        {
-            Console.Write(node.data + " ");
-            last = node;
-            node = node.next;
-        }
-        Console.WriteLine();
-        Console.WriteLine("Traversal in reverse direction: ");
-        while (last != null)
-        {
-            Console.Write(last.data + " ");
-            last = last.prev;
-        }
-    }
+    public string Name { get; init; } = string.Empty;
+    public int Age { get; init; }
 }
 
 public class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        DoublyLinkedList dll = new DoublyLinkedList();
-        dll.InsertEnd(1);
-        dll.InsertFront(2);
-        dll.InsertEnd(3);
-        dll.InsertFront(4);
-
-        Console.WriteLine("Doubly linked list is: ");
-        dll.PrintList(dll.head);
+        Person person = new Person { Name = "Alice", Age = 30 };
+       var data=person with { Age = 31 ,Name="Akshay"};
+        System.Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+        Console.WriteLine($"Name: {data.Name}, Age: {data.Age}");
     }
 }
